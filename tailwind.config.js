861976d23d52639467d492ab72d30336ba4841d4
muhauliautama <muhauliautama@gmail.com>
2023-11-21 12:52:05 +0700

    project selesai

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      borderWidth: {
+        2: "2px",
+      },
+    },
+  },
+  plugins: [],
+}
